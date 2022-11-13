da94d3b2cd27bc2b35185136c2ce4a3d82610f2b
CoasterJX <jianxi1@ualberta.ca>
2022-11-13 00:15:05 -0700

    take as input

--- main.py
+++ main.py
@@ -42,8 +42,6 @@ def main(port):
     
 
 if __name__ == "__main__":
-    if (len(sys.argv) < 2):
-        print(f"Usage: python3 {sys.argv[0]} <port>")
-        exit()
+    port = input("port: ")
     while True:
-        main(sys.argv[1])
+        main(port)
