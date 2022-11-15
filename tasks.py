3beb4376a32fb2c91cf57a515afe0d75998f324a
CoasterJX <jianxi1@ualberta.ca>
2022-11-15 16:54:00 -0700

    add article: multi-author space bug fixed

--- tasks.py
+++ tasks.py
@@ -79,8 +79,65 @@ def list_the_venues(dblp:Collection):
     return
 
 def add_an_article(dblp:Collection):
-    add_article(dblp)
-    return
+    
+    while True:
+        
+        # ask for a unique id
+        _id = input("Please provide a unique id: ")
+        if len(_id) == 0:
+            print('id can not be empty')
+            continue
+        res = dblp.find_one({"id": _id})
+        if res:
+            print("id not unique")
+            continue
+        
+        # ask for a title
+        title = input("Provide a title: ")
+        while len(title) == 0:
+            print("title cannot be empty")
+            title = input("Provide a title: ")
+        
+        # ask for multiple authors
+        authors = []
+        i = 1
+        while True:
+            if i != 1:
+                aut = input(f"Enter Author {i} or press ENTER to finish: ")
+                if aut == "":
+                    break
+            else:
+                aut = input(f"Enter Author {i}: ")
+                if aut == "":
+                    print("Authors cannot be empty. At least 1 author need to be provided.")
+                    continue
+            authors.append(aut)
+            i += 1
+        
+        # ask for year
+        while True:
+            try:
+                year = int(input("Enter year: "))
+                break
+            except ValueError:
+                print("Year is not valid.")
+                continue
+        
+        # insert the new article
+        article = {
+                'abstract': None,
+                'venue': None,
+                'references': [],
+                'n_citations': 0,
+                'id': _id,
+                'title': title,
+                'authors': authors,
+                'year': year
+            }
+        dblp.insert_one(article)
+        print("Add an article successful")
+        break
+
 
 task_dict = {
     "1": search_for_articles,
